9c40f2a0c3186279b91d9a514f7ed764cf373e41
Sherif <sherif@s-arch.local>
2026-08-25 09:13:09 +0300

    Nightly Auto-Build 2026-08-25 09:13:09

--- deploy-nightly.sh
+++ deploy-nightly.sh
@@ -22,7 +22,7 @@ else
 fi
 
 echo "⚙️ [3/4] Compiling Lightspeed Nightly APK..."
-./gradlew assembleDebug --no-daemon
+./gradlew assembleDebug --no-daemon --rerun-tasks
 
 # Immediate cleanup of any transient build daemon, compiler workers, or aapt2 daemons
 killall -9 java aapt2 2>/dev/null || true

5c1ca51d3a0790859c472a3c1f48ae23dd987022
Sherif <sherif@s-arch.local>
2026-08-25 07:14:44 +0300

    Nightly Auto-Build 2026-08-25 07:14:44

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.sbf.lightspeed"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
+        versionCode = 9
         versionName = "1.4.0"
         manifestPlaceholders["appName"] = "Lightspeed"
     }

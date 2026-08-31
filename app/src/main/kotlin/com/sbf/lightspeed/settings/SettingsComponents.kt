5f9229aa8fcf8d7ac9d6a79894e287555aa2d774
Sherif <sherif@s-arch.local>
2026-09-01 00:51:24 +0300

    Nightly Auto-Build 2026-09-01 00:51:24

--- app/src/main/kotlin/com/sbf/lightspeed/settings/SettingsComponents.kt
+++ app/src/main/kotlin/com/sbf/lightspeed/settings/SettingsComponents.kt
@@ -32,6 +32,8 @@ import com.sbf.lightspeed.system.LightspeedPreferences
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures

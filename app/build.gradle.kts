f7792a4d6a264aeb500975cc9817aee0773052e0
Sherif <sherif@s-arch.local>
2026-09-03 21:29:27 +0300

    chore: add lifecycle-viewmodel-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,5 +87,6 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
 }
 
